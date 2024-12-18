f9663cbdb5f8ff151ecc349295cbdb1170f0f416
RyElijah <rsluar@student.apc.edu.ph>
2024-12-18 19:25:52 +0800

    update website

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,9 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "Favorite Motorcycle";
+}
+
+function myFunction2() {
+  const element = document.getElementsByTagName("span"); 
+
+  document.getElementById("demo").innerHTML = 'Its a ' + element[0].innerHTML; 
+}
